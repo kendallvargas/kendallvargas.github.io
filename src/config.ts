0872136965b562b8dd6ca234a993da6c426033f6
kendallvargas <kamakawi8787@gmail.com>
2024-11-20 11:52:02 -0600

    text font

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Kendall Vargas | QA Software Tester';
+export const SITE_DESCRIPTION = 'Personal Website to post information related to my work as a QA Software Tester, related to projects, and any other interesting thing I have to share';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['class', '[data-theme="dark"]'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+		themes: ["lofi", "dark"],
+	}
 }
