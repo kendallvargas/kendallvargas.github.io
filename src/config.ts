b8aeac8f5998342a51530c5b978c330323e4023b
kendallvargas <kamakawi8787@gmail.com>
2024-11-21 08:46:24 -0600

    favicon and social pic

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kendall Vargas | QA Software Tester';
-export const SITE_DESCRIPTION = 'Personal Website to post information related to my work as a QA Software Tester, related to projects, and any other interesting thing I have to share';
+export const SITE_TITLE = 'Kendall Vargas | Software Tester / QA Automation';
+export const SITE_DESCRIPTION = 'Personal Website to post information related to my work as a QA Software Tester, personal projects, blogs, and any other interesting thing I have to share';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
